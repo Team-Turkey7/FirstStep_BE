36d50af9c0a6e51354051bcbaa6fda2409a41a0f
박태수 <taesupark0504@gmail.com>
2025-07-16 01:56:46 +0900

    refactor :: isComplete 값이 변경되고 DB에 반영되도록 리펙토링

--- src/main/java/com/example/firststep_server/domain/problem/service/QueryProblemsByDateService.java
+++ src/main/java/com/example/firststep_server/domain/problem/service/QueryProblemsByDateService.java
@@ -1,7 +1,9 @@
 package com.example.firststep_server.domain.problem.service;
 
 import com.example.firststep_server.domain.problem.domain.Completion;
+import com.example.firststep_server.domain.problem.domain.repository.CompletionRepository;
 import com.example.firststep_server.domain.problem.domain.repository.ProblemRepository;
+import com.example.firststep_server.domain.problem.exception.CompletionNotFoundException;
 import com.example.firststep_server.domain.problem.presentation.dto.response.ProblemResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,11 +15,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class QueryProblemsByDateService {
     private final ProblemRepository problemRepository;
+    private final CompletionRepository completionRepository;
 
-    @Transactional(readOnly = true)
+    @Transactional
     public List<ProblemResponse> execute(String date) {
 
-        Completion completion = new Completion();
+        Completion completion = completionRepository.findByDate(date)
+                .orElseThrow(() -> CompletionNotFoundException.EXCEPTION);
+
         completion.complete();
 
         return problemRepository.findByDateOrderByIdAsc(date);
