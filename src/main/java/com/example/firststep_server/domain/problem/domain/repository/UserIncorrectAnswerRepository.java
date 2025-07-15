20544dafbcdb3540fcdd12dab916f6b693ea6654
oijwef098234 <oijwef098234@gmail.com>
2025-07-16 08:06:47 +0900

    refactor :: JpaRepository로 변경

--- src/main/java/com/example/firststep_server/domain/problem/domain/repository/UserIncorrectAnswerRepository.java
+++ src/main/java/com/example/firststep_server/domain/problem/domain/repository/UserIncorrectAnswerRepository.java
@@ -1,9 +1,10 @@
 package com.example.firststep_server.domain.problem.domain.repository;
 
 import com.example.firststep_server.domain.problem.domain.UserIncorrectAnswer;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserIncorrectAnswerRepository extends CrudRepository<UserIncorrectAnswer,Long> {
+public interface UserIncorrectAnswerRepository extends JpaRepository<UserIncorrectAnswer,Long> {
+
 }
