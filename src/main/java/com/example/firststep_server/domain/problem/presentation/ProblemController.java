cc969fa03d5cdd14cf259720ca0f96946c60bf20
박태수 <taesupark0504@gmail.com>
2025-07-15 19:14:50 +0900

    feat :: judgeProblem API

--- src/main/java/com/example/firststep_server/domain/problem/presentation/ProblemController.java
+++ src/main/java/com/example/firststep_server/domain/problem/presentation/ProblemController.java
@@ -1,7 +1,10 @@
 package com.example.firststep_server.domain.problem.presentation;
 
+import com.example.firststep_server.domain.problem.presentation.dto.request.JudgementProblemRequest;
 import com.example.firststep_server.domain.problem.presentation.dto.response.ProblemResponse;
+import com.example.firststep_server.domain.problem.service.JudgeProblemService;
 import com.example.firststep_server.domain.problem.service.QueryProblemsByDateService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +16,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProblemController {
     private final QueryProblemsByDateService queryProblemsByDateService;
+    private final JudgeProblemService judgeProblemService;
 
     @GetMapping("/{date}")
     @ResponseStatus(HttpStatus.OK)
     public List<ProblemResponse> queryProblemByDate(@PathVariable String date) {
         return queryProblemsByDateService.execute(date);
+    }
+
+    @GetMapping("/judge")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean judgeProblem(@RequestBody @Valid JudgementProblemRequest request) {
+        return judgeProblemService.execute(request);
     }
 }
