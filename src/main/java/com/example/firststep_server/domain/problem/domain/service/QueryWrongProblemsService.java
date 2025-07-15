041086837e56fb2f5e823bbae4299f6b8ea504c8
oijwef098234 <oijwef098234@gmail.com>
2025-07-16 08:06:59 +0900

    feat :: QueryWrongProblemsService

---
+++ src/main/java/com/example/firststep_server/domain/problem/domain/service/QueryWrongProblemsService.java
@@ -0,0 +1,22 @@
+package com.example.firststep_server.domain.problem.domain.service;
+
+import com.example.firststep_server.domain.problem.domain.UserIncorrectAnswer;
+import com.example.firststep_server.domain.problem.domain.repository.UserIncorrectAnswerRepository;
+import com.example.firststep_server.domain.problem.presentation.dto.response.WrongProblemResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class QueryWrongProblemsService {
+    private final UserIncorrectAnswerRepository userIncorrectAnswerRepository;
+
+    public List<WrongProblemResponse> queryWrongProblems() {
+        return userIncorrectAnswerRepository.findAll()
+                .stream()
+                .map(wrongProblemResponses -> new WrongProblemResponse(wrongProblemResponses))
+                .collect(Collectors.toList());
+    }
+}
