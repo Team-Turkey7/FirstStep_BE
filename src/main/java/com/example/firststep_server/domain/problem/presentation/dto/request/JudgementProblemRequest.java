356b4ea13b71dd18ddfac6ca76c8096a70d02b3e
박태수 <taesupark0504@gmail.com>
2025-07-15 19:14:03 +0900

    feat :: JudgementProblemRequest

---
+++ src/main/java/com/example/firststep_server/domain/problem/presentation/dto/request/JudgementProblemRequest.java
@@ -0,0 +1,9 @@
+package com.example.firststep_server.domain.problem.presentation.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class JudgementProblemRequest {
+    private Long id;
+    private String userAnswer;
+}
