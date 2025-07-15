efb9553454d3fbc575668847ffd27203e2bbc6a9
박태수 <taesupark0504@gmail.com>
2025-07-16 01:23:17 +0900

    refactor :: completion entity수정

--- src/main/java/com/example/firststep_server/domain/problem/domain/Completion.java
+++ src/main/java/com/example/firststep_server/domain/problem/domain/Completion.java
@@ -1,11 +1,15 @@
 package com.example.firststep_server.domain.problem.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class Completion {
     @Id
@@ -13,4 +17,10 @@ public class Completion {
     private Long id;
 
     private boolean isComplete;
+
+    private String date;
+
+    public void complete() {
+        this.isComplete = true;
+    }
 }
