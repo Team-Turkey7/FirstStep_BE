13edfe99cf37a02a88be9bc7e4e47adfb0ada092
박태수 <taesupark0504@gmail.com>
2025-07-16 01:35:45 +0900

    refactor :: delete시 url삭제

--- src/main/java/com/example/firststep_server/domain/problem/service/DeleteProblemService.java
+++ src/main/java/com/example/firststep_server/domain/problem/service/DeleteProblemService.java
@@ -1,15 +1,27 @@
 package com.example.firststep_server.domain.problem.service;
 
+import com.example.firststep_server.domain.problem.domain.Problem;
 import com.example.firststep_server.domain.problem.domain.repository.ProblemRepository;
+import com.example.firststep_server.domain.problem.exception.ProblemNotFoundException;
+import com.example.firststep_server.infrastructure.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class DeleteProblemService {
     private final ProblemRepository problemRepository;
+    private final S3Service s3Service;
 
-    public void deleteProblem(Long id){
-        problemRepository.deleteById(id);
+    @Transactional
+    public void deleteProblem(Long id) {
+        Problem problem = problemRepository.findById(id)
+                        .orElseThrow(() -> ProblemNotFoundException.EXCEPTION);
+
+        s3Service.delete(problem.getAudioUrl());
+        s3Service.delete(problem.getPhotoUrl());
+
+        problemRepository.delete(problem);
     }
 }
