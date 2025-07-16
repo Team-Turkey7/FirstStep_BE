395120a93f6ede4da7da35942ec5c20b87dc798b
박태수 <taesupark0504@gmail.com>
2025-07-16 14:09:34 +0900

    feat :: ProblemResponse에 category랑 answer추가

--- src/main/java/com/example/firststep_server/domain/problem/presentation/dto/response/ProblemResponse.java
+++ src/main/java/com/example/firststep_server/domain/problem/presentation/dto/response/ProblemResponse.java
@@ -1,11 +1,15 @@
 package com.example.firststep_server.domain.problem.presentation.dto.response;
 
+import com.example.firststep_server.domain.problem.domain.enums.Category;
+
 public record ProblemResponse(
         Long id,
+        Category category,
         int level,
         String problem,
         String problemDetail,
         String date,
+        String answer,
         String photoUrl,
         String audioUrl
 ) {
