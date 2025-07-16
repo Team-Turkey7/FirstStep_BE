8a03e47eb9786ce13aec22993daf8a423478b3f5
박태수 <taesupark0504@gmail.com>
2025-07-17 08:43:27 +0900

    feat :: findByAnswer query

--- src/main/java/com/example/firststep_server/domain/problem/domain/repository/UserIncorrectAnswerRepository.java
+++ src/main/java/com/example/firststep_server/domain/problem/domain/repository/UserIncorrectAnswerRepository.java
@@ -4,7 +4,9 @@ import com.example.firststep_server.domain.problem.domain.UserIncorrectAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserIncorrectAnswerRepository extends JpaRepository<UserIncorrectAnswer,Long> {
-
+    List<UserIncorrectAnswer> findByAnswer(String answer);
 }
