ae63fb67590f949b01b615df19a0c7fd5baa83fe
박태수 <taesupark0504@gmail.com>
2025-07-16 19:08:07 +0900

    refactor :: category, answer, photoUrl추가

--- src/main/java/com/example/firststep_server/domain/problem/presentation/dto/response/WrongProblemResponse.java
+++ src/main/java/com/example/firststep_server/domain/problem/presentation/dto/response/WrongProblemResponse.java
@@ -1,6 +1,7 @@
 package com.example.firststep_server.domain.problem.presentation.dto.response;
 
 import com.example.firststep_server.domain.problem.domain.UserIncorrectAnswer;
+import com.example.firststep_server.domain.problem.domain.enums.Category;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,13 +14,19 @@ import lombok.NoArgsConstructor;
 public class WrongProblemResponse {
     private Long id;
     private String date;
+    private Category category;
     private String problem;
     private String problemDetail;
+    private String photoUrl;
+    private String answer;
 
     public WrongProblemResponse(UserIncorrectAnswer userIncorrectAnswer) {
         this.id = userIncorrectAnswer.getId();
         this.date = userIncorrectAnswer.getDate();
+        this.category = userIncorrectAnswer.getCategory();
         this.problem = userIncorrectAnswer.getProblem();
         this.problemDetail = userIncorrectAnswer.getProblemDetail();
+        this.photoUrl = userIncorrectAnswer.getPhotoUrl();
+        this.answer = userIncorrectAnswer.getAnswer();
     }
 }
