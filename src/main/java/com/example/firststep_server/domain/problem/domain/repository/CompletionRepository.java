a91761b5febbc334fc3df5e14ac509f295caf497
박태수 <taesupark0504@gmail.com>
2025-07-16 12:08:56 +0900

    feat :: findByDate 쿼리

--- src/main/java/com/example/firststep_server/domain/problem/domain/repository/CompletionRepository.java
+++ src/main/java/com/example/firststep_server/domain/problem/domain/repository/CompletionRepository.java
@@ -4,9 +4,12 @@ import com.example.firststep_server.domain.problem.domain.Completion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CompletionRepository extends JpaRepository<Completion, Long> {
     Optional<Completion> findByDate(String date);
+
+    List<Completion> findAllByDate(String date);
 }
